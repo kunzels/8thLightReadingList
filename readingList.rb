cc59992090a08b64234430486d99032f58643a01
kunzels <kunzelsteven@gmail.com>
2021-06-14 14:07:44 -0400

    project upload

---
+++ readingList.rb
@@ -0,0 +1,130 @@
+require 'byebug'
+require 'tty-prompt'
+require 'rest-client'
+require 'json'
+require 'sqlite3'
+
+class ReadingList
+  
+    def initialize()
+        # Initialize our list, prompt, and key. Populates list from DB insertions. Runs our primary program function.
+        @list = {}
+        @prompt = TTY::Prompt.new
+        @key = ENV['GOOGLE_API_KEY_STEVE_BOOKS']
+        @db = SQLite3::Database.open 'readingList.db'
+        rows = @db.execute <<-SQL
+            create table if not exists bookList (
+            title text,
+            author text,
+            publisher text);
+        SQL
+        @db.execute( "select * from bookList") do |item|
+            @list[item] = true
+        end
+        program()
+    end
+
+    def program()
+        # Main Menu
+        response = @prompt.select("\nWhat would you like to do?\n") do |menu|
+            menu.choice "View My Reading List", "view"
+            menu.choice "Lookup New Books", "lookup"
+            menu.choice "Exit", "exit"
+        end   
+        # Response dictates which function to hit. Program continues running unless exit is selected.
+        case response
+            when "view"
+                viewList() 
+                program()
+            when "lookup"
+                search()
+                program()
+            when "exit"
+                print "\n\n Have a great day! \n\n"
+        end
+    end
+
+    def viewList()
+        if @list.length == 0
+            print "\nYou have no current books in your reading list \n"
+        else # Shovelling (<<) is prefered to += here, alters the original string rather than creating new ones. Our key is an array. 
+            @list.each do |k, v|
+                puts "\n" << "Title: " << k[0] << "\nAuthor/s: " << k[1].to_s << "\nPublisher: " << k[2].to_s << "\n"
+            end
+        end
+    end
+
+    def search() # We may want to allow for even more focused query filtering (title and author and year etc).
+        searchField = @prompt.select("\nHow do you want to search?\n")  do |menu|
+            menu.choice "By Title", "intitle"
+            menu.choice "By Author", "inauthor"
+            menu.choice "By Publisher", "inpublisher"
+            menu.choice "By Subject", "subject"
+            menu.choice "Back", "back"
+            # These are also able to be searched from the API.
+            # menu.choice "By isbn", "isbn"
+            # menu.choice "By lccn", "lccn"
+            # menu.choice "By oclc", "oclc"
+        end
+        if searchField != "back"
+            # Google Books API uses keyword params. These next two lines just remove the "in" from the first three searchField options for clarity to the user, 
+            # since subject does not contain in. Keep this in mind if we add isbn, lccn, or oclc. 
+            term = nil
+            searchField == "subject" ? term = searchField : term = searchField[2..-1]
+            searchTerm = searchInput(term)
+            query(searchField, searchTerm)
+        end
+    end
+
+    def searchInput(term)
+        # User inputs a title, author, publisher, subject. gsub strips spaces and adds '+' for the query params. Sends to the query function.
+        searchTerm = @prompt.ask("\nPlease enter the #{term}:\n")
+        searchTerm == nil ? searchTerm = "" : searchTerm.gsub(/\s+/, "+")
+        return searchTerm
+    end
+    
+    def query(searchField, searchTerm)
+        # Construct URL, send a get request to the api, parse. maxResults added. See api documentation for url param clarity.  
+        url = "https://www.googleapis.com/books/v1/volumes?q=#{searchField}:#{searchTerm}&maxResults=5&projection=lite&:keyes&key=#{@key}"
+        response = JSON.parse(RestClient.get(url))
+        # If there arent results, else display the received options.
+        if response.length < 3
+            puts "\nThere were no selections found\n"
+        else
+            # display results to choose from
+            selection = @prompt.select("\nSelect an item to add to your reading list\n")  do |menu|
+                response["items"].each do |book|
+                    bookInfo = book["volumeInfo"]
+                    title = bookInfo["title"]
+                    authors = bookInfo["authors"]
+                    publisher = bookInfo["publisher"]
+                    menu.choice "Title: #{title}, Author/s:#{authors.to_s}, Publisher: #{publisher}", [title, authors, publisher]
+                end
+                menu.choice "Back", "back"
+            end
+            # Chooses where to send the selection
+            if selection != "back"
+                listAndDbInsert(selection)
+            else selection == "back"
+                search()
+            end
+        end
+    end
+
+    def listAndDbInsert(selection)
+        # Formats for insertion and inclusion check
+        selection[1] == nil ? selection[1] = "No listed author" : selection[1] = selection[1].join(", ")
+        selection[2] = "No listed publisher" if selection[2] == nil 
+        # If unadded, adds to list and db
+        if !@list[selection]
+            @list[selection] = true
+            @db.execute "INSERT INTO bookList (title, author, publisher) VALUES (?, ?, ?)", selection[0], selection[1], selection[2]
+            print "\n#{selection} has been added to your reading list\n"
+        else 
+            print "\nThis is already in your reading list\n"
+            search()
+        end
+    end
+end
+
+test = ReadingList.new()
---
+++ readme.md
@@ -0,0 +1,60 @@
+Google Books API and Reading List
+
+A small command-line program designed to query google books API with a simple reading list attached.
+
+Goals for this project:
+Type in a query and display a list of 5 books matching that query.
+Each item in the list should include the book's author, title, and publishing company.
+A user should be able to select a book from the five displayed to save to a “Reading List”
+View a “Reading List” with all the books the user has selected from their queries -- this is a local reading list and not tied to Google Books account features.
+
+To run
+
+1. Navigate to 8thLightStevenKunzel in the terminal.
+2. Run bundle install in the console to install relevant gems.
+3. Create an environment variable using your google books API key (run in terminal: export GOOGLE_API_KEY_STEVE_BOOKS=key). You can choose a different environment variable name by altering line 13.
+4. Run ruby readingList.rb in the console. 
+
+Approach
+My approach was to initialize our class with a hash for the reading list, initialize the prompt gem, and run a program function. I also used an sqlite3 database for some light information retainment. A database will be created if none exists, and it will be opened if it already does exist. A table is created called bookList if not already created, with some columns for title, author, and publisher. We start the program by pulling from this DB and putting everything into a reference hash called list.
+
+Passing to the program function, we are given a selection to view our list, look up new books, or exit.
+
+Upon viewing, we run viewlist. This either returns an empty list message or displays our hash's keys with some formatting. It then returns to program, and reruns it.
+
+Selecting lookup runs the search function. The search function displays a list of filter options and a back button. Back will return us to program, and rerun program. A variable term is used, because there is a difference in the query parameter between subject and other search's (all others contain "in" in the search param).
+
+Upon creating a proper searchField, it moves to the searchInput function to retrieve the user input. Here we have a small error catch if the user just hits enter without typing, it will just do a blank search. This could be altered to prompt the user that they must enter information.
+
+It returns the search term to the search function, moving to the query function.
+
+The query functions interpolates the passed searchField and searchTerm into a URL adding a limit of 5 to the max results, and utilizing google API's "projection=lite" param to make the records smaller in size, including less irrelevant information. It runs a query to the API, parsing the data into JSON. We check if the length is greater than 3 because anything less indicates no results. If there are results, we will display to the user the results found, including the title, author, and publisher. If the user selects back, the search is run again, otherwise, they can select an item.
+
+Selecting an item sends the selection to the listAndDbInsert function. This formats the selection to check if it is already present in the @list hash. If it isn't, it inserts and executes a database insertion.
+
+Resources
+
+Accessing the google books API was done through the aid of the rest-client gem. You can see this link for documentation on rest-client.
+https://github.com/rest-client/rest-client
+
+The google books API documentation is here.
+https://developers.google.com/books/docs/overview
+
+I also installed a helpful integration called tty-prompt. This allows for some easy-to-manage UI improvements. See documentation here.
+https://github.com/piotrmurach/tty-prompt
+
+Sqlite3 documentation, specific to ruby. Some more references are linked here as well.
+https://rubydoc.info/gems/sqlite3/1.3.8/frames
+
+Byebug is included. This would be removed on a production build, but it is included if you'd like to use it. Simply place debugger on a line. See documentation here.
+https://github.com/deivid-rodriguez/byebug
+
+Future considerations
+
+1. Add tests. I decided against tests for this project due to time constraints, but RSPEC could be utilized for a spec based testing suite.
+2. Make the list more readable, perhaps a nice-looking table that organizes everything.
+3. Ability to delete from the reading list, or check off completed books to move to a finished list, etc.
+4. Filter further, by title AND author, by year, etc.
+5. User login integration.
+6. Next button for queries to allow choices beyond just 5.
+7. Better key security.
